644b4becfff09d076951fce35da6ebaeb04f12ce
Anatoliy K <778966@mail.ru>
2022-09-19 23:05:23 +0500

    added ex1

---
+++ Example01/Program.cs
@@ -0,0 +1,12 @@
+﻿void WriteHello(string name)
+{
+    Console.WriteLine("Hello, " + name);
+}
+WriteHello(Console.ReadLine());
+
+string HelloToWord(string name)
+{
+    return ("Hello, " + name);
+}
+String word = HelloToWord ("Tolya");
+Console.WriteLine(word + " как дела?");
