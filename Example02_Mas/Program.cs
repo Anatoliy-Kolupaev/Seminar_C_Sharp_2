3bea7847bfce674b5a0497fc4cb8c70d377656cc
Anatoliy K <778966@mail.ru>
2022-09-19 23:54:15 +0500

    added ex2

---
+++ Example02_Mas/Program.cs
@@ -0,0 +1,25 @@
+﻿//int [] numbers = new int[10]
+//int[] a = {1, 2, 3, 5, 6, 7, 8, 9, 0};
+//System.Console.WriteLine(a[5]);
+//1- что мы хотим получить (void - ничего, int-числовой массив, string - строка)
+//2- название функции (любое)
+//3- что мы хотим отдать
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size]; // создаем массив задавая размер через size
+    for (int i = 0; i < size; i++) // функция фор читай сам) заполнили
+    {
+        array[0] = i+1;
+    }
+    return array; //отдаем массив
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+int[] numbers = CreateArray(10);
+WriteArray (numbers);
